7a25198c140ceb807f8b4de785d06627272982a8
Hamza-Ziani <hamzaziani381@gmail.com>
2024-09-24 10:33:24 +0100

    'ebank backs no security'

---
+++ src/main/java/com/ebanking_backs/ebanking_backs/controllers/BankAccountController.java
@@ -0,0 +1,60 @@
+package com.ebanking_backs.ebanking_backs.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+import com.ebanking_backs.ebanking_backs.dtos.*;
+import com.ebanking_backs.ebanking_backs.exceptions.*;
+import com.ebanking_backs.ebanking_backs.services.BankAccountService;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@CrossOrigin("*")
+public class BankAccountController {
+    
+    private BankAccountService bankAccountService;
+
+    public BankAccountController(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+    
+
+    @GetMapping("/accounts/{accountId}")
+    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        return bankAccountService.getBankAccount(accountId);
+    }
+    @GetMapping("/accounts")
+    public List<BankAccountDTO> listAccounts(){
+        return bankAccountService.bankAccountList();
+    }
+    @GetMapping("/accounts/{accountId}/operations")
+    public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
+        return bankAccountService.accountHistory(accountId);
+    }
+
+    @GetMapping("/accounts/{accountId}/pageOperations")
+    public AccountHistoryDTO getAccountHistory(
+            @PathVariable String accountId,
+            @RequestParam(name="page",defaultValue = "0") int page,
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
+        return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
+    }
+    @PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return creditDTO;
+    }
+    @PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
+    }
+}
---
+++ src/main/java/com/ebanking_backs/ebanking_backs/controllers/CustomerRestController.java
@@ -0,0 +1,46 @@
+package com.ebanking_backs.ebanking_backs.controllers;
+
+import java.util.List;
+
+import com.ebanking_backs.ebanking_backs.dtos.*;
+import com.ebanking_backs.ebanking_backs.exceptions.CustomerNotFoundException;
+import com.ebanking_backs.ebanking_backs.services.BankAccountService;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin("*")
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers(){
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers("%"+keyword+"%");
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+        customerDTO.setId(customerId);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        bankAccountService.deleteCustomer(id);
+    }
+
+    
+}
